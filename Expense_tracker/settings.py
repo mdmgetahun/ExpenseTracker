2a85bca28b84161085e9ab23f403cbe579bbf2a3
mdgetahun <henokmgetahun@gmail.com>
2025-10-18 22:28:03 +0200

    Prepare django project for deployment

--- Expense_tracker/settings.py
+++ Expense_tracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-unmyf6^-ln^qlx)brlg!23a25htr@hl@fd(i573$r#e4$cyb^a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
